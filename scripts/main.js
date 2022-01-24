9a331e8ea434554c2e408b61dcd09bbc3fce7209
Valence707 <valence707@gmail.com>
2022-01-23 21:13:52 -0500

    Clean up design, improve JS

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,11 @@
 // Stores objects containing word & filtered information.
-let words = [];
+let wordCollection = [];
 
-// Uses dictionary api to populate words array.
-let grabWord = () => {
+/* Grabs information from the Dictionary and Thesaurus APIs about a particular
+word and returns that information as an object. */
+function grabWord(word, speechPart) {
 
-    // Grabs value of user word input and that word's part of speech.
-    let word = $("#userTextInput").val().toLowerCase();
-    let partOfSpeech = $("#userSpeechPartInput").val().toLowerCase();
-
-    // Stores word's 
+    // Stores word information.
     let finalWord = {
         "word": word,
         "def1": ``,
@@ -24,7 +21,7 @@ let grabWord = () => {
     $.getJSON(`https://dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=0d870a14-7aad-4361-8131-c69a997d9a2c`,
         function (dictData) {
             for (let wordVar of dictData) {
-                if (wordVar.fl == partOfSpeech && finalWord.def1 == "") {
+                if (wordVar.fl == speechPart && finalWord.def1 == "") {
                     finalWord.def1 = wordVar.shortdef[0];
                     finalWord.def2 = wordVar.shortdef[1];
         
@@ -40,7 +37,7 @@ let grabWord = () => {
     $.getJSON(`https://dictionaryapi.com/api/v3/references/thesaurus/json/${word}?key=a51b46ac-7b10-485c-bef5-0f7715f9eab7`,
         function (thesData) {
             for (let wordVar of thesData) {
-                if (wordVar.fl == partOfSpeech) {
+                if (wordVar.fl == speechPart) {
                     finalWord.syns1 = [...wordVar.meta.syns[0].slice(0, 3)];
 
                     finalWord.syns2 = [...wordVar.meta.syns[0].slice(3, 6)];
@@ -49,8 +46,37 @@ let grabWord = () => {
         }
     );
 
-    words.push(finalWord);
+    // Returns object containing collected word's information.
+    return finalWord;
+}
 
+function populateDisplay() {
+    let words = $(`#userTextInput`).val()
+    .replace(/[\s\d]/gi, "")
+    .split(",");
+
+    let partsOfSpeech = $(`#userSpeechPartInput`).val()
+    .replace(/[\s\d]/gi, "")
+    .split(",");
+
+    // call grabWord() for each user-inputted word.
+    // Populate wordCollection array with word objects returned by each grabWord call.
+    // Use an interval to delay calling of grabWord to make Firefox stop crashing
+    // because I'm making too many calls to the API or something.
+    if (words.length == partsOfSpeech.length) {
+        let tempCounter = 0;
+        let populateWords = setInterval(function() {
+            console.log(words.length);
+            console.log(wordCollection.length);
+            wordCollection.push(grabWord(words[tempCounter], partsOfSpeech[tempCounter]));
+            tempCounter++;
+            if (words.length == wordCollection.length) {
+                console.log(wordCollection);
+                clearInterval(populateWords);
+            }
+        }, 1000);
+    }
+    
     // Clears text input fields.
     $(`#userTextInput`).val("");
     $(`#userSpeechPartInput`).val("");
